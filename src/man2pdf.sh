0c0987430fb809ebebc5af327ce1b0d1d371ccea
ytxmobile98 <5900105+ytxmobile98@users.noreply.github.com>
2023-03-05 23:41:08 +0800

    docs: optional outPath

--- src/man2pdf.sh
+++ src/man2pdf.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
+# Check input arguments
 manpage="$1"
 outPath="$2"
-
-# Check input arguments
 if [ -z "$manpage" ]
 then
     echo "Usage: bash \"$0\" <man_page_name> [out_file.pdf]" >&2
     exit 1
 fi
+# Note: The outPath is optional. If it is not given, it will be resolved automatically according to the manpage name.
 
 # Check ps2pdf
 if ! which ps2pdf > /dev/null
